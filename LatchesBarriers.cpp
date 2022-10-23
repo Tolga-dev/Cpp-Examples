1a74d9bbd4ea51f494d69b492d51a8da95d8692f
Your Name <you@example.com>
2022-10-23 14:52:32 +0300

    latches barries will add semapho

---
+++ LatchesBarriers.cpp
@@ -0,0 +1,12 @@
+//#include <barrier>
+//#include <latch>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
+
+int main() {
+
+
+    return 0;
+}
---
+++ Semaphores.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <semaphore.h>
+
+int main()
+{
+
+
+    return 0;
+}
