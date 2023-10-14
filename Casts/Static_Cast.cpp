ca0835c3ba4c8b0faa23d64df30135650ebe149f
Tolga-dev <orhan.konat.12@gmail.com>
2023-10-14 11:57:56 +0300

    junk

--- Casts/Static_Cast.cpp
+++ Casts/Static_Cast.cpp
@@ -1,3 +1,32 @@
-//
-// Created by xamblot on 12/10/22.
-//
+#include <bits/c++io.h>
+#include <stdio.h>
+#include <iostream>
+
+
+
+using namespace std;
+
+class Int
+{
+    int x;
+
+public:
+    Int(int k = 0) : x{k} { cout << " Constructor " << endl; }
+    operator string()    { cout << " Conversion " << endl;  return to_string(x); }
+
+
+};
+
+
+int main()
+{
+    Int obj(97);
+
+    string s = obj;
+
+    string s2 = static_cast<string>(obj);
+    obj  = static_cast<Int>(96);
+
+    std::cout << s << std::endl;
+
+}
--- main.cpp
+++ main.cpp
@@ -1,3 +1,28 @@
-//
-// Created by xamblot on 12/4/22.
-//
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define print(m) std::cout << m << std::endl;
+#define SumArr(arr,n) int size = sizeof(arr) / sizeof(arr[0]); \
+    for(int i = 0; i<= size; i++){      \
+        n += arr[i]; }
+#define sum(i,j) i+j
+#define rep(i,a,b) for(int i = a; i <= b; i++)
+using namespace std;
+typedef long long ll;
+typedef vector<pair<int, int>>  vpii;
+typedef vector<int>  vi;
+typedef set<int>  si;
+typedef multiset<int>  mtsi;
+typedef map<string,int>  mpsi;
+
+
+int main()
+{
+
+    return 0;
+
+}
+
+
